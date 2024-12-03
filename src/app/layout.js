29374bc37b6c34e51f911fb77ade3a4d68518455
muradov2004 <ami65143@gmail.com>
2024-12-03 11:45:53 +0400

    Updated seo

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import localFont from "next/font/local";
 import "./globals.css";
+import Head from "next/head";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -20,6 +21,51 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <title>
+          C-Means Clustering Calculator | Visualize Data Clusters Online
+        </title>
+
+        <meta
+          name="description"
+          content="Perform C-Means clustering online with this easy-to-use calculator. Ideal for data analysis, fuzzy logic, and machine learning applications."
+        />
+
+        <meta
+          name="keywords"
+          content="C-Means Clustering Calculator, Fuzzy C-Means Calculator, Data Clustering Online, Machine Learning Clustering Tool, Unsupervised Learning Techniques"
+        />
+
+        <meta property="og:title" content="C-Means Clustering Calculator" />
+        <meta
+          property="og:description"
+          content="Perform and visualize C-Means clustering online. Simplify your data analysis tasks with our advanced calculator."
+        />
+        <meta
+          property="og:url"
+          content="https://muradov2004.github.io/c-means/"
+        />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:image"
+          content="https://muradov2004.github.io/c-means/favicon.ico"
+        />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="C-Means Clustering Calculator" />
+        <meta
+          name="twitter:description"
+          content="Easily perform C-Means clustering with this online calculator. Designed for machine learning and data science."
+        />
+        <meta
+          name="twitter:image"
+          content="https://muradov2004.github.io/c-means/favicon.ico"
+        />
+
+        <meta name="robots" content="index, follow" />
+
+        <link rel="canonical" href="https://muradov2004.github.io/c-means/" />
+      </Head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,6 @@
 import { useState } from "react";
 import CalculationArea from "@/components/CalculationArea";
 import Table from "@/components/Table";
-import Head from "next/head";
 
 export default function Home() {
   const defaultRow = { x: "", y: "", c1: "", c2: "" };
@@ -14,51 +13,6 @@ export default function Home() {
 
   return (
     <div className="min-h-screen font-[family-name:var(--font-geist-sans)]">
-      <Head>
-        <title>
-          C-Means Clustering Calculator | Visualize Data Clusters Online
-        </title>
-
-        <meta
-          name="description"
-          content="Perform C-Means clustering online with this easy-to-use calculator. Ideal for data analysis, fuzzy logic, and machine learning applications."
-        />
-
-        <meta
-          name="keywords"
-          content="C-Means Clustering Calculator, Fuzzy C-Means Calculator, Data Clustering Online, Machine Learning Clustering Tool, Unsupervised Learning Techniques"
-        />
-
-        <meta property="og:title" content="C-Means Clustering Calculator" />
-        <meta
-          property="og:description"
-          content="Perform and visualize C-Means clustering online. Simplify your data analysis tasks with our advanced calculator."
-        />
-        <meta
-          property="og:url"
-          content="https://muradov2004.github.io/c-means/"
-        />
-        <meta property="og:type" content="website" />
-        <meta
-          property="og:image"
-          content="https://muradov2004.github.io/c-means/favicon.ico"
-        />
-
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="C-Means Clustering Calculator" />
-        <meta
-          name="twitter:description"
-          content="Easily perform C-Means clustering with this online calculator. Designed for machine learning and data science."
-        />
-        <meta
-          name="twitter:image"
-          content="https://muradov2004.github.io/c-means/favicon.ico"
-        />
-
-        <meta name="robots" content="index, follow" />
-
-        <link rel="canonical" href="https://muradov2004.github.io/c-means/" />
-      </Head>
       <main className="flex flex-col text-center">
         <Table rows={rows} setRows={setRows} setShow={setShow} />
         {show && <CalculationArea rows={rows} />}
